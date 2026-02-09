24f67db7783f5fc3de702af0557ab309e6a945af
HuzefaMD <45355976+huzefaMD@users.noreply.github.com>
2026-02-09 20:01:28 +0530

    Create 17_functionarg.sh for user welcome function
    Add a script to welcome users with their names and ages.

---
+++ ShellScripts/17_functionarg.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+function welcomenote {
+
+        echo "-------"
+        echo "Welcome $1"
+        echo "Age is $2"
+        echo "--------"
+
+}
+
+welcomenote Raju 20
+welcomenote Sam 30
