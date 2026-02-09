d3330044ea3d5a6bd121d2e215d8c185cdced559
HuzefaMD <45355976+huzefaMD@users.noreply.github.com>
2026-02-09 19:57:02 +0530

    Add for loop script to print numbers and names

---
+++ ShellScripts/14_forloop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+for i in 1 2 3 4 5 6 7 8 9 10
+
+do
+        echo "Number is $i"
+done
+
+for name in Ajit Sharif ram Subash Vasanth Vamshi Siva Karthikeyan Mahesh Vinoth Ashiq Dhanu Ebin huzefa
+
+do
+        echo "Name is $name"
+done
