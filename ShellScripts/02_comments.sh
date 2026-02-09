38460d389f899815d299ee1aeb1663c2c202f5e3
HuzefaMD <45355976+huzefaMD@users.noreply.github.com>
2026-02-09 19:36:36 +0530

    Add script for demonstrating comments in Bash

---
+++ ShellScripts/02_comments.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+#This is a command in linux
+echo "checking comments"
+
+#This is a single line comment.
+
+
+<<comment
+This
+is
+multi
+line comments
+class is
+going on
+comment
+
+#this command shows the date
+date
