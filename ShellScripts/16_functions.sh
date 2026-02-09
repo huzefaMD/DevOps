75b1ce6c5e6ddb695bfc0710887481da39d3668d
HuzefaMD <45355976+huzefaMD@users.noreply.github.com>
2026-02-09 19:59:42 +0530

    Create a welcome note function in 16_functions.sh
    Add a function to print a welcome message multiple times.

---
+++ ShellScripts/16_functions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#To make the function script
+
+function welcomenote {
+
+        echo "-------"
+        echo "Welcome"
+        echo "-------"
+
+}
+
+#to call our function
+#
+welcomenote
+welcomenote
+welcomenote
+welcomenote
+welcomenote
+
