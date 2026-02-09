ea852bee0c245b789ab66d1ea0f49670297ac2f3
HuzefaMD <45355976+huzefaMD@users.noreply.github.com>
2026-02-09 19:56:07 +0530

    Create 13_logocal.sh for voting eligibility check
    Add a script to check voting eligibility based on age and country.

---
+++ ShellScripts/13_logocal.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+#AND Operator
+#
+read -p "what is your age?" age
+read -p "your country:" country
+
+if [[ $age -ge 18 ]] || [[ $country == "India" ]]
+
+then
+        echo "You can vote"
+
+else
+        echo "you can't vote"
+
+fi
