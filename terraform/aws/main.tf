6177cab33ade00b2c63132b560bc9fecde20fbff
HuzefaMD <45355976+huzefaMD@users.noreply.github.com>
2026-03-11 06:43:07 +0000

    Enable public IP mapping and set availability zone

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -27,6 +27,8 @@ resource "aws_vpc" "TF-VPC" {
 resource "aws_subnet" "name" {
     vpc_id = aws_vpc.TF-VPC.id
     cidr_block = "10.0.1.0/24"
+    map_public_ip_on_launch = true
+    availability_zone = "us-east-1a"
 }
 
 resource "aws_internet_gateway" "gw" {
